70dca18f8f9dd2fcf2c604fe31da1e38a9ecd4f0
Andrew Broman <abroman@medicine.wisc.edu>
2016-04-25 15:46:48 -0500

    Complete lessons through #5

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import NavLink from './NavLink';
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+      <div>
+        <h1>Github Issues</h1>
+        <ul role="nav">
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
+        </ul>
+
+        { this.props.children }
+      </div>
+    );
   }
 })
