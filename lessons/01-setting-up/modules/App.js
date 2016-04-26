a91eed2e7dfbb99b854010fe86237f5d1fa88545
Andrew Broman <abroman@medicine.wisc.edu>
2016-04-26 14:32:11 -0500

    Add Home IndexRoute

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavLink from './NavLink';
+import { IndexLink, Link } from 'react-router';
 
 export default React.createClass({
   render() {
@@ -7,6 +8,8 @@ export default React.createClass({
       <div>
         <h1>Github Issues</h1>
         <ul role="nav">
+          <li><IndexLink to="/" activeClassName="active">Home via IndexLink</IndexLink></li>
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home via NavLink</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
